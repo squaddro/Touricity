dd586fcc5bf124881edecfaf793616c8fcb6f088
Yasar Can <ykakdas@gmail.com>
2020-01-13 18:48:19 +0300

    Cookie kontrolcüsü projeye eklendi.

--- app/src/main/java/com/squadro/touricity/MainActivity.java
+++ app/src/main/java/com/squadro/touricity/MainActivity.java
@@ -1,18 +1,29 @@
 package com.squadro.touricity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
 
+import com.squadro.touricity.cookie.CookieMethods;
 import com.squadro.touricity.view.map.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = getApplicationContext();
         setContentView(R.layout.activity_main);
-        LinearLayout viewById = (LinearLayout)findViewById(R.id.main_activity);
-        viewById.addView(new MapView(getApplicationContext()));
+        LinearLayout viewById = (LinearLayout) findViewById(R.id.main_activity);
+        viewById.addView(new MapView(context));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CookieMethods.cleanCookies();
     }
 }
--- app/src/main/java/com/squadro/touricity/retrofit/RetrofitCreate.java
+++ app/src/main/java/com/squadro/touricity/retrofit/RetrofitCreate.java
@@ -1,13 +1,28 @@
 package com.squadro.touricity.retrofit;
 
+import com.squadro.touricity.cookie.AddCookiesInterceptor;
+import com.squadro.touricity.cookie.ReceivedCookiesInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitCreate {
 
     public Retrofit createRetrofit() {
+
+        OkHttpClient.Builder okHttpClient = new OkHttpClient().newBuilder()
+                .connectTimeout(60 * 5, TimeUnit.SECONDS)
+                .readTimeout(60 * 5, TimeUnit.SECONDS)
+                .writeTimeout(60 * 5, TimeUnit.SECONDS);
+        okHttpClient.interceptors().add(new AddCookiesInterceptor());
+        okHttpClient.interceptors().add(new ReceivedCookiesInterceptor());
+
         return new Retrofit.Builder()
                 .baseUrl("buraya url yazilacak") // TODO: server tarafi halledildiginde gercek url'i yaz buraya
+                .client(okHttpClient.build())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
