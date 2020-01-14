63bf92b20a9ce0f79ef06d1ba28b466dcca411dd
Yasar Can <ykakdas@gmail.com>
2020-01-15 00:03:27 +0300

    Map view eklendi.

--- app/src/main/java/com/squadro/touricity/MainActivity.java
+++ app/src/main/java/com/squadro/touricity/MainActivity.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import android.widget.LinearLayout;
 
 import com.squadro.touricity.cookie.CookieMethods;
 import com.squadro.touricity.view.PanelLayout;
 import com.squadro.touricity.view.filter.FilterSearchView;
+import com.squadro.touricity.view.map.MapFragment;
 import com.squadro.touricity.view.map.MapView;
 import com.squadro.touricity.view.routeList.RouteListView;
 
@@ -28,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
         int mainWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
 
         manageView(linearLayout, new FilterSearchView(context), mainWidth, (int) (mainHeight * 0.2));
+
+        MapFragment mapFragment = new MapFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        getSupportFragmentManager().executePendingTransactions();
+
+
         manageView(linearLayout, new MapView(context), mainWidth, (int) (mainHeight * 0.5));
         manageView(linearLayout, new RouteListView(context), mainWidth, (int) (mainHeight * 0.3));
     }
