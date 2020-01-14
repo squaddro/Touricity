ab35612b99cc5e0b54481bad81ae18a2141d1f40
BatuhanUlucay <hulucay@etu.edu.tr>
2020-01-14 03:07:15 +0300

    bugfix

--- app/src/main/java/com/squadro/touricity/message/types/Stop.java
+++ app/src/main/java/com/squadro/touricity/message/types/Stop.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Stop extends Entry implements IStop {
+public class Stop extends AbstractEntry implements IStop {
 
     private String location_id;
     private String stop_id;
