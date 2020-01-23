d7c2e506a6d3a2a1135657bae5367822d9be7d1d
Yasar Can <ykakdas@gmail.com>
2020-01-23 19:35:16 +0300

    Previous implementation is removed.

--- app/src/main/java/com/squadro/touricity/MainActivity.java
+++ app/src/main/java/com/squadro/touricity/MainActivity.java
@@ -1,17 +1,10 @@
 package com.squadro.touricity;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
 
 import com.squadro.touricity.cookie.CookieMethods;
-import com.squadro.touricity.view.PanelLayout;
-import com.squadro.touricity.view.filter.FilterSearchView;
-import com.squadro.touricity.view.map.MapFragment;
-import com.squadro.touricity.view.map.MapView;
-import com.squadro.touricity.view.routeList.RouteListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,31 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         context = getApplicationContext();
         setContentView(R.layout.activity_main);
-        LinearLayout linearLayout = findViewById(R.id.main_activity);
-
-        int mainHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
-        int mainWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-
-        manageView(linearLayout, new FilterSearchView(context), mainWidth, (int) (mainHeight * 0.2));
-
-        MapFragment mapFragment = new MapFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
-        getSupportFragmentManager().executePendingTransactions();
-
-
-        manageView(linearLayout, new MapView(context), mainWidth, (int) (mainHeight * 0.5));
-        manageView(linearLayout, new RouteListView(context), mainWidth, (int) (mainHeight * 0.3));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         CookieMethods.cleanCookies();
-    }
-
-    private void manageView(LinearLayout mainLayout, PanelLayout panelLayout, int width, int height) {
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
-        panelLayout.setLayoutParams(layoutParams);
-        mainLayout.addView(panelLayout);
     }
 }
--- app/src/main/java/com/squadro/touricity/view/routeList/RouteListView.java
+++ app/src/main/java/com/squadro/touricity/view/routeList/RouteListView.java
@@ -1,67 +1,14 @@
 package com.squadro.touricity.view.routeList;
 
 import android.content.Context;
-import android.widget.ExpandableListView;
 
 import com.squadro.touricity.R;
 import com.squadro.touricity.view.PanelLayout;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 public class RouteListView extends PanelLayout {
-
-    RouteExpandableListAdapter listAdapter;
-    ExpandableListView expandableListView;
-    List<String> dataHeaderList;
-    HashMap<String, List<RouteListItem>> childDataList;
 
     public RouteListView(Context context) {
         super(context);
-        initializeRouteListView(context);
-    }
-
-    private void initializeRouteListView(Context context) {
-        expandableListView = (ExpandableListView) findViewById(R.id.expandable_list_view);
-
-        ExpandableListViewListeners listeners = new ExpandableListViewListeners(context,expandableListView);
-
-        expandableListView.setOnGroupClickListener(listeners.getOnGroupClickListener());
-        expandableListView.setOnGroupExpandListener(listeners.getOnGroupExpandListener());
-        expandableListView.setOnGroupCollapseListener(listeners.getOnGroupCollapseListener());
-        expandableListView.setOnChildClickListener(listeners.getOnChildClickListener());
-
-        // preparing list data
-        prepareListData();
-
-        //our custom adapter for handling data
-        listAdapter = new RouteExpandableListAdapter(context, dataHeaderList, childDataList);
-
-        // setting list adapter
-        expandableListView.setAdapter(listAdapter);
-    }
-
-    private void prepareListData() {
-        // Some dummy data for testing listview
-
-        dataHeaderList = new ArrayList<String>();
-        childDataList = new HashMap<String, List<RouteListItem>>();
-
-        // Adding child data
-        dataHeaderList.add("Header 1");
-        dataHeaderList.add("Header 2");
-        dataHeaderList.add("Header 3");
-
-        // Adding child data
-
-        List<RouteListItem> childList1 = new ArrayList<>();
-        List<RouteListItem> childList2 = new ArrayList<>();
-        List<RouteListItem> childList3 = new ArrayList<>();
-
-        childDataList.put(dataHeaderList.get(0), childList1);
-        childDataList.put(dataHeaderList.get(1), childList2);
-        childDataList.put(dataHeaderList.get(2), childList3);
     }
 
     @Override
