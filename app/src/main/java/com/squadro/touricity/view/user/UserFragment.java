905f49d7a929349ee64568c77f8d8189be91ce06
beyzanurgencetu <b.genc@etu.edu.tr>
2020-03-07 00:10:04 +0300

    register ve login templateları yazıldı.

--- app/src/main/java/com/squadro/touricity/view/user/UserFragment.java
+++ app/src/main/java/com/squadro/touricity/view/user/UserFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.squadro.touricity.R;
 
@@ -15,7 +16,23 @@ public class UserFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.user_page_view, container, false);
+        View v = inflater.inflate(R.layout.user_page_view, container, false);
+        TextView signUp = (TextView) v.findViewById(R.id.link_signup);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                v.findViewById(R.id.signin_layout).setVisibility(View.INVISIBLE);
+                v.findViewById(R.id.signup_layout).setVisibility(View.VISIBLE);
+            }
+        });
+
+        TextView login = (TextView) v.findViewById(R.id.link_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                v.findViewById(R.id.signin_layout).setVisibility(View.VISIBLE);
+                v.findViewById(R.id.signup_layout).setVisibility(View.INVISIBLE);
+            }
+        });
+        return v;
     }
 
 
