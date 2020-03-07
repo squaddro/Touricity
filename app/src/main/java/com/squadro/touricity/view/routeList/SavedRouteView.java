496fa5284ea02721e9cc4a53a40a823a3650811e
BatuhanUlucay <hulucay@etu.edu.tr>
2020-03-07 15:54:52 +0300

    saved tabi icin rota cizdirme eklendi.

--- app/src/main/java/com/squadro/touricity/view/routeList/SavedRouteView.java
+++ app/src/main/java/com/squadro/touricity/view/routeList/SavedRouteView.java
@@ -1,27 +1,37 @@
 package com.squadro.touricity.view.routeList;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.squadro.touricity.R;
 import com.squadro.touricity.message.types.Route;
+import com.squadro.touricity.view.routeList.event.IRouteDraw;
 
 import java.util.List;
 
 import lombok.Getter;
+import lombok.Setter;
+
 @RequiresApi(api = Build.VERSION_CODES.M)
 
-public class SavedRouteView extends LinearLayout{
+public class SavedRouteView extends LinearLayout implements ScrollView.OnScrollChangeListener {
 
     @Getter
     private List<Route> routeList;
 
+    @Setter
+    private IRouteDraw iRouteDraw;
+
+    private Route prevHighlighted;
     LinearLayout routes;
     NestedScrollView scrollView;
 
@@ -58,5 +68,24 @@ public class SavedRouteView extends LinearLayout{
 
         routes = findViewById(R.id.route_save_list);
         scrollView = findViewById(R.id.route_save_scroll);
+        scrollView.setOnScrollChangeListener(this);
+    }
+
+    @Override
+    public void onScrollChange(View view, int i, int i1, int i2, int i3) {
+        Rect rect = new Rect();
+        scrollView.getHitRect(rect);
+        for(int j=0; j<routes.getChildCount(); j++) {
+            RouteCardView routeView = (RouteCardView) routes.getChildAt(i);
+            Route route = routeView.getRoute();
+            if(routeView.getLocalVisibleRect(rect)){
+                if(route != null && (prevHighlighted != route)){
+                    iRouteDraw.drawHighlighted(route);
+                }
+                prevHighlighted = route;
+
+                break;
+            }
+        }
     }
 }
