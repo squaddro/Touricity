81b26e75f9524fa3b5bf080bc6cad0f81f77625b
BatuhanUlucay <hulucay@etu.edu.tr>
2020-04-01 03:21:21 +0300

    stoplar icin comperator eklendi

---
+++ app/src/main/java/com/squadro/touricity/view/map/DirectionsAPI/StopComparator.java
@@ -0,0 +1,14 @@
+package com.squadro.touricity.view.map.DirectionsAPI;
+
+import com.squadro.touricity.message.types.Stop;
+
+import java.util.Comparator;
+
+class StopComparator implements Comparator<Stop> {
+
+    @Override
+    public int compare(Stop stop, Stop t1) {
+
+        return stop.getIndex() - t1.getIndex();
+    }
+}
