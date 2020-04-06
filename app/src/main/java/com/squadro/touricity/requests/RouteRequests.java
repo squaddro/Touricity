6ce27544d7f3c435778eb74c9833bac5ba35bb17
beyzanurgencetu <b.genc@etu.edu.tr>
2020-04-06 20:35:56 +0300

    route insert with user info

--- app/src/main/java/com/squadro/touricity/requests/RouteRequests.java
+++ app/src/main/java/com/squadro/touricity/requests/RouteRequests.java
@@ -6,8 +6,10 @@ import android.support.annotation.RequiresApi;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.squadro.touricity.MainActivity;
 import com.squadro.touricity.converter.RouteConverter;
 import com.squadro.touricity.message.types.Route;
+import com.squadro.touricity.message.types.RouteRegister;
 import com.squadro.touricity.retrofit.RestAPI;
 import com.squadro.touricity.retrofit.RetrofitCreate;
 import com.squadro.touricity.view.routeList.IRouteResponse;
@@ -26,16 +28,21 @@ public class RouteRequests {
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void updateRoute(Route route, IRouteResponse iRouteResponse) {
         RouteConverter routeConverter = new RouteConverter();
+        RouteRegister routeRegister = new RouteRegister();
 
         route.setCreator("94ca3876-9bd1-41ec-ad1d-36dadaf3b024");
         route.setCity_id("111111111111111111111111111111111111");
         route.setPrivacy(2);
+
+        routeRegister.setUsername(MainActivity.credential.getUser_name());
+        routeRegister.setRoute(route);
+
         RetrofitCreate retrofitCreate = new RetrofitCreate();
         Retrofit retrofit = retrofitCreate.createRetrofit();
         RestAPI restAPI = retrofit.create(RestAPI.class);
 
         Gson gson = new Gson();
-        String jsonString = gson.toJson(route);
+        String jsonString = gson.toJson(routeRegister);
         jsonString = jsonString.replace("abstractEntryList", "entries");
         JsonParser jsonParser = new JsonParser();
         JsonObject asJsonObject = jsonParser.parse(jsonString).getAsJsonObject();
