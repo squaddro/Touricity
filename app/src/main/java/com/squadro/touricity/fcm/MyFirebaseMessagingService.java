af508ee7decb6709b5134ccec107a0b4285fdd76
beyzanurgencetu <b.genc@etu.edu.tr>
2020-04-08 17:33:52 +0300

    method implemented for notification

--- app/src/main/java/com/squadro/touricity/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/squadro/touricity/fcm/MyFirebaseMessagingService.java
@@ -1,9 +1,13 @@
 package com.squadro.touricity.fcm;
+import android.app.Notification;
 import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.squadro.touricity.R;
 
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
@@ -18,7 +22,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle(remoteMessage.getData().get("title"))
+                .setContentText(remoteMessage.getData().get("body"))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+        manager.notify(123, notification);
     }
+
     //call this method for fcm token
     public static String getToken(Context context) {
         return context.getSharedPreferences("_", MODE_PRIVATE).getString("fcm_token", "empty");
