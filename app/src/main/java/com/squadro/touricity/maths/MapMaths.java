3ef33dde2e5d8b3d5cc1f17d727572c21e779c8c
BatuhanUlucay <hulucay@etu.edu.tr>
2020-04-18 13:37:58 +0300

    path ve rota icin uzunluk fonksiyonlari eklendi.

--- app/src/main/java/com/squadro/touricity/maths/MapMaths.java
+++ app/src/main/java/com/squadro/touricity/maths/MapMaths.java
@@ -9,6 +9,7 @@ import com.squadro.touricity.message.types.Route;
 import com.squadro.touricity.message.types.Stop;
 import com.squadro.touricity.message.types.interfaces.IEntry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapMaths {
@@ -163,4 +164,28 @@ public class MapMaths {
         }
         return MapMaths.boundsPadding(builder.build(), 10, 10, 60, 10);
     }
+
+    public static double distanceOfPath(Path path){
+        double distance = 0.0;
+        List<PathVertex> vertices = path.getVertices();
+        int firstIndex = 0;
+        int secondIndex = 1;
+
+        while(secondIndex < vertices.size()){
+            distance += distance(vertices.get(firstIndex).toLatLong(), vertices.get(secondIndex).toLatLong());
+        }
+        return distance;
+    }
+
+    public static double distanceOfRoute(Route route){
+        double distance = 0.0;
+
+        for (IEntry entry:route.getAbstractEntryList()){
+            if(entry instanceof Path){
+                distance += distanceOfPath((Path) entry);
+            }
+        }
+        return distance;
+    }
+
 }
